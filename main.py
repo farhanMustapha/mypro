0e142a52d17cdf360a484353019ef8703a3ed8db
Fakhita123* <farhan.mustapha2016@gmail.com>
2024-11-26 21:36:09 +0100

    11 eme

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import flet as ft
 import json
+import streamlit as st
+import subprocess
+
+# Exemple de mise à jour de pip
+if st.button('Mettre à jour pip'):
+    result = subprocess.run(['pip', 'install', '--upgrade', 'pip'], capture_output=True, text=True)
+    st.text(result.stdout)  # Affiche la sortie de la commande
+    st.text(result.stderr)  # Affiche les erreurs (le cas échéant)
 
 
 
--- req.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import subprocess
-
-# Exemple de mise à jour de pip
-if st.button('Mettre à jour pip'):
-    result = subprocess.run(['pip', 'install', '--upgrade', 'pip'], capture_output=True, text=True)
-    st.text(result.stdout)  # Affiche la sortie de la commande
-    st.text(result.stderr)  # Affiche les erreurs (le cas échéant)
